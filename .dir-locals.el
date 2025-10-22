3584dabd999466f91f824ce6211dafc5e597b62b
sejgit <5984155+sejgit@users.noreply.github.com>
2025-10-22 06:45:28 -0400

    project support files

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((python-mode
+  . ((python-shell-interpreter . "/Users/stephen/.local/share/virtualenvs/mqtt-control-abc123/bin/python")
+     (lsp-pyright-venv-path . "/Users/stephen/.local/share/virtualenvs")
+     (lsp-pyright-venv-name . "mqtt-control-abc123"))))
