9793979ef7acdfd83365c5bc7fdea8ed1430d90b
Stephen Jenkins <sejgit@github.com>
2025-10-22 16:14:39 -0400

    generic project files fixed

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,15 @@
 ((python-mode
-  . ((python-shell-interpreter . "/Users/stephen/.local/share/virtualenvs/mqtt-control-abc123/bin/python")
-     (lsp-pyright-venv-path . "/Users/stephen/.local/share/virtualenvs")
-     (lsp-pyright-venv-name . "mqtt-control-abc123"))))
+  . ((eval . (let ((venv (getenv "VIRTUAL_ENV")))
+               (when (and venv (file-exists-p venv))
+                 (let ((venv-name (file-name-nondirectory venv)))
+                   (setq python-shell-interpreter (concat venv "/bin/python"))
+                   (setq lsp-pyright-venv-path venv)
+                   (setq lsp-pyright-venv-name venv-name)))))))
+
+ (python-ts-mode
+  . ((eval . (let ((venv (getenv "VIRTUAL_ENV")))
+               (when (and venv (file-exists-p venv))
+                 (let ((venv-name (file-name-nondirectory venv)))
+                   (setq python-shell-interpreter (concat venv "/bin/python"))
+                   (setq lsp-pyright-venv-path venv)
+                   (setq lsp-pyright-venv-name venv-name))))))))
